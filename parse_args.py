1ae9a3fcf5ca8cc90c8202cb4dd1033e18de2cea
Anand Singh <iamtheanandsingh@gmail.com>
2025-10-29 16:54:28 -0500

    codeprep

---
+++ parse_args.py
@@ -0,0 +1,59 @@
+from argparse import ArgumentParser
+
+# Add Help when finished
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--run_name', type=str, default='', help='', 
+    )   
+    parser.add_argument(
+        '--gradient_accumulation_steps', type=int, default=1, help='', 
+    )
+    parser.add_argument(
+        '--project_name', type=str, default='DocRes', help='', 
+    )
+    parser.add_argument(
+        '--seed', type=int, default=0, help='', 
+    )
+    parser.add_argument(
+        '--batch_size', type=int, default=8, help=''
+    )
+    
+    parser.add_argument(
+        '--alpha', default=1e-2, type=float, help=''
+    )
+    
+    parser.add_argument(
+        '--image_size', type=int, default=288, help='', 
+    )
+    parser.add_argument(
+        '--learning_rate', type=float, default=1e-4, help='', 
+    )
+    parser.add_argument(
+        '--warmup_steps', type=int, default=1000, help='', 
+    )
+    parser.add_argument(
+        '--num_epochs', type=int, default=10, help='', 
+    )
+    parser.add_argument(
+        '--log_freq', type=int, default=1, help='', 
+    )
+    parser.add_argument(
+        '--save_freq', type=int, default=100, help='', 
+    )
+    parser.add_argument(
+        '--eval_bz', type=int, default=256, help='', 
+    )
+    parser.add_argument(
+        '--output_dir', type=str, default='logs', help='', 
+    )
+    parser.add_argument(
+        '--dataset_name', type=str, default='./data/doc3d', help='', 
+    )
+    parser.add_argument(
+        '--gpu_ids', type=str, default='0,1', help='', 
+    )
+    
+    
+    #bool is "action='store_true'"
+    return parser.parse_args()
